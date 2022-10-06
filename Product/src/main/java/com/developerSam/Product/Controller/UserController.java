d388a5835999ce54d263bb22435018451ef8e86f
AirlineRepository <gitongas^84@gmail.com>
2022-10-06 23:27:14 +0300

    Is a simple airline booking system
    Signed-off-by: AirlineRepository <gitongas^84@gmail.com>

--- Product/src/main/java/com/developerSam/Product/Controller/UserController.java
+++ Product/src/main/java/com/developerSam/Product/Controller/UserController.java
@@ -2,11 +2,9 @@ package com.developerSam.Product.Controller;
 
 import com.developerSam.Product.Entity.AirlineCustomer;
 import com.developerSam.Product.Repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
